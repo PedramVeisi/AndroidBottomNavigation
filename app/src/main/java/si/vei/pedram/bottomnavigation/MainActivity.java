92c12d62ceb8fc06da3fae8c1c29074ac273e0ff
Pedram Veisi <pedram.veisi@clio.com>
2017-06-23 22:56:22 -0600

    anchor the popup menu on more button

--- app/src/main/java/si/vei/pedram/bottomnavigation/MainActivity.java
+++ app/src/main/java/si/vei/pedram/bottomnavigation/MainActivity.java
@@ -8,8 +8,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.view.menu.MenuPopupHelper;
 import android.support.v7.widget.PopupMenu;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener,
@@ -17,33 +22,66 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
     private TextView mTextMessage;
 
+    private PopupWindow mPopupWindow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
         BottomNavigationViewHelper.disableShiftMode(navigation);
 
 
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        PopupMenu popupMenu = new PopupMenu(MainActivity.this, fab);
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, navigation, Gravity.RIGHT);
         popupMenu.inflate(R.menu.global_create_menu);
 
         //noinspection RestrictedApi
         final MenuPopupHelper menuHelper = new MenuPopupHelper(MainActivity.this,
-                (MenuBuilder) popupMenu.getMenu(), fab);
+                (MenuBuilder) popupMenu.getMenu(), navigation.findViewById(R.id.navigation_more));
         menuHelper.setForceShowIcon(true);
+        menuHelper.setGravity(Gravity.RIGHT);
+
+        menuHelper.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                fab.setVisibility(View.VISIBLE);
+            }
+        });
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 menuHelper.show();
+                //mPopupWindow.showAtLocation(navigation, Gravity.BOTTOM, 0, 0);
+                fab.setVisibility(View.GONE);
             }
         });
+
+
+
+
+//        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+//
+//        View customView = inflater.inflate(R.layout.global_create, null);
+//
+//        mPopupWindow = new PopupWindow(
+//                customView,
+//                ViewGroup.LayoutParams.WRAP_CONTENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT
+//        );
+
+//        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+//            @Override
+//            public void onDismiss() {
+//                fab.setVisibility(View.VISIBLE);
+//            }
+//        });
+
 
     }
 
